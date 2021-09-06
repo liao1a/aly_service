037a456527ad77743764439a6d33fb6ab2bc200b
liao1a <153887111@qq.com>
2021-09-06 10:32:51 +0800

    nginx 增加https配置

--- python/run.sh
+++ python/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# pip install --upgrade pip && pip install -i https://mirrors.aliyun.com/pypi/simple/ -r /docker/requirements.txt
+# pip install --upgrade pip && pip install -i https://mirrors.aliyun.com/pypi/simple/ -r /docker/requirements.txt && pip install numpy && pip install json
 
 # 运行API入口脚本
 python /app/test.py
